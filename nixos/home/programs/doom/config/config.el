28937531c867b7040d1d97d754669450c391d2f0
lolwierd <lolwierd@outlook.com>
2024-05-23 15:54:06 +0530

    we lightin

--- nixos/home/programs/doom/config/config.el
+++ nixos/home/programs/doom/config/config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-challenger-deep)
+;; (setq doom-theme 'doom-challenger-deep)
+(setq doom-theme 'doom-one-light)
 
 ;; start emacs maximized
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
