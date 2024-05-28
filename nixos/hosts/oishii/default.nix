28423ceb22cbf0c5f21504584742d350a61d9dcc
lolwierd <lolwierd@outlook.com>
2024-05-28 13:43:49 +0530

    we dont need no firewall

--- nixos/hosts/oishii/default.nix
+++ nixos/hosts/oishii/default.nix
@@ -58,8 +58,9 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # networking.firewall = {
   # allowedUDPPorts = [ 54834 ];
