3db43b1d76f779afc2502f2206272e306847582c
lolwierd <lolwierd@outlook.com>
2024-06-04 10:38:18 +0530

    twas renamed m'lord

--- nixos/modules/gnome/default.nix
+++ nixos/modules/gnome/default.nix
@@ -17,8 +17,8 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "lolwierd";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "lolwierd";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
