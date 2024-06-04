1f325d3f4a3e1308bdfd6bb837da053f4c8cf50c
lolwierd <lolwierd@outlook.com>
2024-06-04 12:30:46 +0530

    we do dconf change

--- nixos/home/gnome/dconf.nix
+++ nixos/home/gnome/dconf.nix
@@ -5,8 +5,12 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "org/gnome/Extensions" = {
+      window-maximized = true;
+    };
+
     "org/gnome/control-center" = {
-      last-panel = "display";
+      last-panel = "keyboard";
       window-state = mkTuple [ 980 640 false ];
     };
 
@@ -33,17 +37,59 @@ with lib.hm.gvariant;
       translate = true;
     };
 
+    "org/gnome/desktop/datetime" = {
+      automatic-timezone = false;
+    };
+
     "org/gnome/desktop/input-sources" = {
       sources = [ (mkTuple [ "xkb" "us" ]) ];
       xkb-options = [ "caps:swapescape" ];
+    };
+
+    "org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/privacy" = {
+      old-files-age = mkUint32 30;
+      recent-files-max-age = -1;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      sort-order = [ "org.gnome.Contacts.desktop" "org.gnome.Documents.desktop" "org.gnome.Nautilus.desktop" ];
     };
 
     "org/gnome/desktop/session" = {
       idle-delay = mkUint32 600;
     };
 
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-monitor-down = [ "<Shift><Super>Down" ];
+      move-to-workspace-left = [ "<Shift><Control><Super>Left" ];
+      move-to-workspace-right = [ "<Shift><Control><Super>Right" ];
+      show-desktop = [ "<Super>d" ];
+      switch-applications = [];
+      switch-applications-backward = [];
+      switch-to-workspace-left = [ "<Control><Super>Left" ];
+      switch-to-workspace-right = [ "<Control><Super>Right" ];
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+    };
+
+    "org/gnome/epiphany" = {
+      ask-for-default = false;
+    };
+
     "org/gnome/evolution-data-server" = {
       migrated = true;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
     };
 
     "org/gnome/nautilus/preferences" = {
@@ -56,9 +102,15 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell" = {
+      disabled-extensions = [ "drive-menu@gnome-shell-extensions.gcampax.github.com" "system-monitor@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" ];
+      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
       favorite-apps = [ "org.gnome.Nautilus.desktop" "kitty.desktop" "chromium-browser.desktop" "code.desktop" ];
       last-selected-power-profile = "performance";
       welcome-dialog-last-shown-version = "46.2";
+    };
+
+    "org/gnome/shell/extensions/appindicator" = {
+      icon-contrast = 0.4;
     };
 
     "org/gnome/shell/world-clocks" = {
