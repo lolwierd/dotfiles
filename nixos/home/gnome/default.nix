fd79a20d2f13377a68e7c8cd46a9d77ac89e95a6
lolwierd <lolwierd@outlook.com>
2024-06-21 11:04:21 +0530

    this mfer where did it come from?

--- nixos/home/gnome/default.nix
+++ nixos/home/gnome/default.nix
@@ -36,7 +36,7 @@
   ];
 
   # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+  # systemd.user.startServices = "sd-switch";
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
