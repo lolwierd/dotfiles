b9fc27a1cf564cf467fcbb7491d74d497e78fc73
lolwierd <lolwierd@outlook.com>
2024-06-21 11:11:03 +0530

    this mfer does not work for some reason

--- nixos/home/programs/nvim/config/lua/lolwierd/lazy.lua
+++ nixos/home/programs/nvim/config/lua/lolwierd/lazy.lua
@@ -94,18 +94,6 @@ require("lazy").setup({
   },
   "lambdalisue/suda.vim",
   {
-    "shortcuts/no-neck-pain.nvim",
-    version = "*",
-    config = require("no-neck-pain").setup({
-      width = 120,
-      fallbackOnBufferDelete = true,
-      autocmds = {
-        enableOnVimEnter = true,
-        reloadOnColorSchemeChange = true,
-      },
-    }),
-  },
-  {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
