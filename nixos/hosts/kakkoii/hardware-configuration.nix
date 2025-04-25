bc54109136dd1e51785e1c8802c10199cf74ae95
lolwierd <lolwierd@outlook.com>
2025-04-25 10:41:15 +0530

    me dumb :(

--- nixos/hosts/kakkoii/hardware-configuration.nix
+++ nixos/hosts/kakkoii/hardware-configuration.nix
@@ -8,23 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d5a47d7a-f40a-4b7b-8342-fdea4a9712ed";
+    { device = "/dev/disk/by-uuid/fed82e7a-b3f7-4c92-9e45-8cdd11462407";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6CB5-A8A0";
+    { device = "/dev/disk/by-uuid/9F22-BE8C";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/e414e721-6510-429d-b364-3b9b43f2830b"; } ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
